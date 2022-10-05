fc25a699325649bec313cabe1b458d0df58d6d7b
jkc-mycode <jkc9709@naver.com>
2022-10-05 16:00:47 +0900

    Update source

--- app/src/main/java/kr/ac/kumoh/s20201414/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20201414/w0501carddealer/MainActivity.kt
@@ -2,7 +2,9 @@ package kr.ac.kumoh.s20201414.w0501carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.s20201414.w0501carddealer.databinding.ActivityMainBinding
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
+
+        binding.card1.setImageResource(R.drawable.c_10_of_diamonds)
+        getCardName(32)
+    }
+
+    private fun getCardName(c: Int) : String{
+
+        val shape = when(c/13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            else -> "error"
+        }
+        Log.i("getCardName", shape)
+        return "c_4_of_spades"
     }
 }
