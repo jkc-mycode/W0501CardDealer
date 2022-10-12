b61e2d6e0c056282ec2ecf808391e5ab9ec4c978
jkc-mycode <jkc9709@naver.com>
2022-10-12 15:16:49 +0900

    Update source

--- app/src/main/java/kr/ac/kumoh/s20201414/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20201414/w0501carddealer/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import kr.ac.kumoh.s20201414.w0501carddealer.databinding.ActivityMainBinding
 import kotlin.math.log
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,8 +16,35 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
-        binding.card1.setImageResource(R.drawable.c_10_of_diamonds)
-        getCardName(32)
+        //binding.card1.setImageResource(R.drawable.c_10_of_diamonds)
+
+        /*
+        binding.btnDeal.setOnClickListener{
+            val res = resources.getIdentifier(getCardName(Random.nextInt(52)), "drawable", packageName);
+            binding.card1.setImageResource(res)
+            binding.card2.setImageResource(res)
+            binding.card3.setImageResource(res)
+            binding.card4.setImageResource(res)
+            binding.card5.setImageResource(res)
+        }
+         */
+        binding.btnDeal.setOnClickListener{
+            val c = IntArray(5)
+            val res = IntArray(5)
+
+
+            for(i in res.indices){
+                c[i] = Random.nextInt(52)
+                res[i] = resources.getIdentifier(getCardName(c[i]), "drawable", packageName)
+            }
+            binding.card1.setImageResource(res[0])
+            binding.card2.setImageResource(res[1])
+            binding.card3.setImageResource(res[2])
+            binding.card4.setImageResource(res[3])
+            binding.card5.setImageResource(res[4])
+        }
+
+
     }
 
     private fun getCardName(c: Int) : String{
@@ -25,9 +53,18 @@ class MainActivity : AppCompatActivity() {
             0 -> "spades"
             1 -> "diamonds"
             2 -> "hearts"
+            3 -> "clubs"
             else -> "error"
         }
         Log.i("getCardName", shape)
-        return "c_4_of_spades"
+        val number = when (c % 13){
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        return "c_${number}_of_${shape}"
     }
 }
